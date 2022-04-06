75d0f14c2138c2c4ceebf296df02d985ae6ab74f
Vaisvik-Chaudhary <vaisvik.chaudhary@bacancy.com>
2022-04-06 18:29:01 +0530

    Crud operation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_105019) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_05_044832) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -24,8 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_04_105019) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "dob"
-    t.integer "age"
+    t.date "date_of_birth"
     t.string "address"
     t.string "contact"
   end
